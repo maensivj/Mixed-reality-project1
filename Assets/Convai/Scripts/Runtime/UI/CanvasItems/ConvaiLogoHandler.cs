82a67773f3e0ccc3a420336c08e42762f813e3eb
B100per <siriwatwattana51@gmail.com>
2025-02-10 10:07:28 +0200

    add AI component

---
+++ Assets/Convai/Scripts/Runtime/UI/CanvasItems/ConvaiLogoHandler.cs
@@ -0,0 +1,32 @@
+using Convai.Scripts.Runtime.UI.SettingsPanel;
+using UnityEngine;
+
+namespace Convai.Scripts.Runtime.UI
+{
+    public class ConvaiLogoHandler : MonoBehaviour
+    {
+        [SerializeField] private GameObject convaiLogo;
+
+        private void Start()
+        {
+            ChangeLogo(false);
+        }
+
+        private void OnEnable()
+        {
+            ChatUIBase.UIStatusChange += ChangeLogo;
+            UIAppearanceSettings.UIStatusChange += ChangeLogo;
+        }
+
+        private void OnDisable()
+        {
+            ChatUIBase.UIStatusChange -= ChangeLogo;
+            UIAppearanceSettings.UIStatusChange -= ChangeLogo;
+        }
+
+        private void ChangeLogo(bool isHidden)
+        {
+            convaiLogo.SetActive(!isHidden);
+        }
+    }
+}
---
+++ Assets/Convai/Scripts/Runtime/UI/SettingsPanel/UISettingsPanelOpenButton.cs
@@ -0,0 +1,25 @@
+using Convai.Scripts.Runtime.UI.SettingsPanel;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Convai.Scripts.Runtime.UI
+{
+    public class UISettingsPanelOpenButton : MonoBehaviour
+    {
+        private Button _openButton;
+        private UISettingsPanel _uiSettingsPanel;
+
+        private void Awake()
+        {
+            _uiSettingsPanel = FindObjectOfType<UISettingsPanel>();
+            _openButton = GetComponent<Button>();
+
+            _openButton.onClick.AddListener(OpenSettingsPanel);
+        }
+
+        private void OpenSettingsPanel()
+        {
+            _uiSettingsPanel.ToggleSettingsPanel(true);
+        }
+    }
+}
