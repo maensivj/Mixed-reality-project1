20994719852e85776c8b77615707d61c3471c8ea
maensivj <126621342+maensivj@users.noreply.github.com>
2025-03-02 20:45:55 +0200

    screwing

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,13 +7,15 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody rb;
     private bool isAtCorrectPosition = false;
     public GameObject screwdriver;
+    private float ogYPosition;
+    public GameObject targetPos;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ogYPosition = transform.position.y;
 
-       
-        
+
     }
     void OnTriggerEnter(Collider other)
     {
@@ -22,21 +24,25 @@ public class NewBehaviourScript : MonoBehaviour
             isAtCorrectPosition = true;
             Debug.Log("colliding");
         }
-        if (other.CompareTag("Screwdriver"))
-        {
-            isAtCorrectPosition = true;
-            Debug.Log("colliding");
-        }
+        
     }
     // Update is called once per frame
     void Update()
     {
         if (isAtCorrectPosition)
         {
-            transform.Rotate(Vector3.forward * 100f * Time.deltaTime);
+            
+            
+            if (transform.localPosition.y > targetPos.transform.localPosition.y)
+            {
+                Debug.Log("0");
+                transform.Rotate(Vector3.forward * 150f * Time.deltaTime);
+                Debug.Log("1");
+                transform.position += new Vector3(0, -0.01f * Time.deltaTime, 0);
+                Debug.Log("2");
+            }
 
         }
-        //transform.Rotate(Vector3.forward * 100f * Time.deltaTime);
-        //transform.Rotate(Vector3.up * 100f * Time.deltaTime);
+        
     }
 }
